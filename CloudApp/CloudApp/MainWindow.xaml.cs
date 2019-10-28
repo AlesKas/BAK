f4cafa3caa3938fbdd68bba3fe6b4660c9c78bf2
AlesKas <ales.kasparek01@upol.cz>
2019-10-28 20:02:38 +0100

    Initial commint

---
+++ CloudApp/CloudApp/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CloudApp
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(username.Text) || String.IsNullOrEmpty(password.Password))
+            {
+                MessageBox.Show("Please enter username and password.", "Error", MessageBoxButton.OK);
+            }
+            else
+            {
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(@"Data Source=localhost;Initial Catalog=CloudUsers;Integrated Security=True"))
+                    {
+                        conn.Open();
+                        SqlCommand command = new SqlCommand("SELECT * FROM cloud WHERE ");
+
+                        using (SqlDataReader dr = command.ExecuteReader())
+                        {
+                            while (dr.Read())
+                            {
+
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK);
+                }
+            }
+        }
+    }
+}
---
+++ CloudApp/CloudApp/SQLhandler.cs
@@ -0,0 +1,44 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CloudApp
+{
+    class SQLhandler
+    {
+        private string username { get; }
+        private string password { get; }
+        private string DBusername { get; set; }
+        private string DBpassword { get; set; }
+
+        public bool getCreditals()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(@"Data Source=localhost;Initial Catalog=CloudUsers;Integrated Security=True"))
+                {
+                    conn.Open();
+                    SqlCommand command = new SqlCommand("SELECT * FROM cloud WHERE ");
+
+                    using (SqlDataReader dr = command.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK);
+                return false;
+            }
+            return true;
+        }
+    }
+}
