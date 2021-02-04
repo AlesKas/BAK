1bcb9ead252ee77de27184d0aa09ff29e97d5a74
AlesKas <akaspare@redhat.com>
2021-02-04 20:11:13 +0100

    FUCKING SHIT

--- Desktop/Desktop/Desktop/cApp.h
+++ Desktop/Desktop/Desktop/cApp.h
@@ -1,6 +1,10 @@
 #pragma once
 
-#include "wx/wx.h"
+#ifdef _WIN64
+	#include "wx/wx.h"
+#elif __linux__
+	#include <wx-3.1/wx/wx.h>
+#endif
 #include "cMain.h"
 
 class cApp : public wxApp
--- Desktop/Desktop/Desktop/cMain.h
+++ Desktop/Desktop/Desktop/cMain.h
@@ -1,5 +1,11 @@
 #pragma once
-#include "wx/wx.h"
+#ifdef _WIN64
+	#include "wx/wx.h"
+#elif __linux__
+	#include <wx-3.1/wx/wx.h>
+#endif
+#include "cMain.h"
+
 
 class cMain : public wxFrame
 {
