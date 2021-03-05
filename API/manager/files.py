8b3022f8a27af8198e451e4215d449b158e3644a
AlesKas <ales.kasparek@email.cz>
2021-03-05 15:05:46 +0100

    File upload and download handling

--- API/manager/files.py
+++ API/manager/files.py
@@ -1,13 +1,50 @@
+import os
+
+from utils.logger import initLogging
+from utils.disk_util import DISK_PATH
 from .base import PutRequest, GetRequest
+from flask import send_from_directory, make_response
+
+LOGGER = initLogging()
 
 class FilePutHandler(PutRequest):
 
     @classmethod
     def handle_put(cls, **kwargs):
+        filePath = DISK_PATH + kwargs["user"]
+        file = kwargs["fileName"]
+        file.save(os.path.join(filePath, file.filename))
         return 200
 
 class FileGetHandler(GetRequest):
 
     @classmethod
     def handle_get(cls, **kwargs):
-        return 200
+        response = {}
+        response["data"] = []
+        currentUser = kwargs["user"]
+        userWorkspace = DISK_PATH + currentUser
+        for file in os.listdir(userWorkspace):
+            fileResp = {}
+            base = os.path.basename(file)
+            fileName, fileType = os.path.splitext(base)
+            fileResp["fileName"] = fileName
+            if fileType == "":
+                fileResp["fileType"] = "dir"
+            else:
+                fileResp["fileType"] = fileType[1:]
+            response["data"].append(fileResp)
+
+        response["data"] = sorted(response["data"], key=lambda k: k["fileType"])
+        return response
+
+class FileGetHandlerDownload(GetRequest):
+
+    @classmethod
+    def handle_get(cls, **kwargs):
+        fileName = kwargs["fileName"]
+        currentUser = kwargs["user"]
+        userWorkspace = DISK_PATH + currentUser + "/"
+        response = make_response(send_from_directory(userWorkspace, fileName, as_attachment=True))
+        response.direct_passthrough = False
+        return response
--- API/utils/disk_util.py
+++ API/utils/disk_util.py
@@ -1,7 +1,7 @@
 import os
 
-DISK_PARH = "/tmp/NAS/"
+DISK_PATH = "/tmp/NAS/"
 
 def create_user_folder(userName: str):
-    user_path = DISK_PARH + userName
+    user_path = DISK_PATH + userName
     os.mkdir(user_path)
