b5b9ca99f05757f00247c933700fe79b8ee66244
AlesKas <ales.kasparek@email.cz>
2021-03-26 12:47:13 +0100

    install and run sript

---
+++ install.sh
@@ -0,0 +1,10 @@
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker already installed."
+    # command
+else
+    echo "Installing docker"
+    curl -fsSL get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+fi
+echo "Installing docker-compose"
+pip3 install docker-compose
